68b0108ef852eae79662180f50da15ce355b2f78
Damien Delforge <dam.delforge@gmail.com>
2024-06-20 00:16:31 +0200

    Add library details to vassal/__init__.py
    Expanded the __init__.py file in the vassal directory to include library details such as version, authors, and license. This will provide a clear view of the library information when imported.

--- vassal/__init__.py
+++ vassal/__init__.py
@@ -1,1 +1,14 @@
+"""Visual and Automated Singular Spectrum Analysis Library (VASSAL)
+
+Author: Damien Delforge <damien.delforge@adscian.be>
+        Alice Alonso <alice.alonso@adscian.be>
+License: BSD-3-Clause
+
+"""
 from .ssa import SingularSpectrumAnalysis
+
+__version__ = '0.1.0'
+__author__ = ('Damien Delforge <damien.delforge@adscian.be>, '
+              'Alice Alonso <alice.alonso@adscian.be>')
+__license__ = 'BSD-3-Clause'
+__all__ = ['SingularSpectrumAnalysis']
