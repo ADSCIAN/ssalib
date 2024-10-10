0652671f5f568930ad5962ccf4ade5b7bcbd0e85
Damien Delforge <dam.delforge@gmail.com>
2024-10-10 14:40:35 +0200

    Add scipy and sklearn to dependencies in requirements.txt
    Updated the requirements.txt to include scipy and sklearn as mandatory dependencies. Introduced a function in setup.py to parse dependencies directly from the requirements.txt file for better maintainability. Also corrected the author information and provided a more descriptive project name.

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    return [line.strip() for line in lines if
+            line.strip() and not line.startswith('#')]
+
 setup(
     name='vassal',
     version='0.0.1',
@@ -9,16 +16,12 @@ setup(
     },
     url='https://github.com/ADSCIAN/vassal',
     license='BSD-3-Clause',
-    author='Damien Delforge',
+    author='Damien Delforge, Alice Alonso',
     author_email='damien.delforge@adscian.be',
-    description='Visual and Automated Singular Spectrum Analysis Library',
+    description='Visual and Automated Singular Spectrum Analysis (VASSAL)',
     long_description=open('README.md').read(),
-    install_requires=[
-        'numpy',
-        'matplotlib',
-        'pandas',
-    ],
+    install_requires=parse_requirements('requirements.txt'),
     extras_require={
-        'full': ['scipy', 'scikit-learn', 'dask']
+        'full': ['dask']
     }
 )
